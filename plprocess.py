90b1c9445f86ca77ed30dcc818c6a30faf645595
nathanningbo <498235584@qq.com>
2018-03-01 14:15:05 +0800

    Integration of preprocessing

--- plprocess.py
+++ plprocess.py
@@ -3,7 +3,30 @@ import shutil
 import re
 # path = 'D:\\BigTruck\\new\\'
 # newpath ='D:\\BigTruck\\new2\\'
-def images_copy_and_rename(oldpath, newpath ,imagetype, imgstartnum):
+#1.rootpath为数据根目录，要求原始图像放在rootpath/raw下
+#2.rootpath下不允许有images路径，以及train.txt文件
+#3.rootpath/raw下各种类别的文件名需要按照数字排序好
+#4.rootpath/images为图像输出路径，同时rootpath/images下会生成相应的label文件
+def dataproces(rootpath):
+    if (not os.path.exists(rootpath)):
+        print('rootpath error')
+        return 0
+    RawImgRootPath = os.path.join(rootpath,'raw')
+    if (not os.path.exists(RawImgRootPath)):
+        print('RawImgRootPath error')
+        return 0
+    ImgRootPath = os.path.join(rootpath,'images')
+    startnum =1
+    if (not os.path.exists( ImgRootPath)):
+        os.mkdir( ImgRootPath);
+    for file in os.listdir(RawImgRootPath):
+        rawimgtypespath = os.path.join(RawImgRootPath, file)
+        imgtypespath = os.path.join( ImgRootPath, file)
+        if os.path.isfile(rawimgtypespath) == False:
+            startnum = images_copy_and_rename(rawimgtypespath, imgtypespath, '.jpg', startnum)
+    direct_gen_text(ImgRootPath ,rootpath)
+
+def images_copy_and_rename(oldpath, newpath ,imagetype , imgstartnum = 1):
     count = imgstartnum
     if(not os.path.exists(oldpath)):
         return 0
@@ -15,16 +38,15 @@ def images_copy_and_rename(oldpath, newpath ,imagetype, imgstartnum):
                 newname = str(count) +imagetype
                 shutil.copyfile(os.path.join(oldpath, file), os.path.join(newpath, newname))
                 count += 1
-                print( file )
     print( '路径下有'+ str(count)+'张'+imagetype)
-    return 1
+    return count
 def gen_img_label_text( imgpath, outfilepath, outfilename ,labelnum):
     f = open(os.path.join(outfilepath, outfilename), 'w')
     for filename in os.listdir(imgpath):
         f.write(filename + labelnum+'\n')
     f.close()
 
-#imgrootpath 图片根目录
+#生成txt文件
 def direct_gen_text(imgrootpath ,txtpath, txtname = 'train.txt' ,imgtype = '.jpg'):
     count = 1 #每循环完一次之后，图片id的初始位置
     type = 1  #label的值
@@ -37,6 +59,7 @@ def direct_gen_text(imgrootpath ,txtpath, txtname = 'train.txt' ,imgtype = '.jpg
         count += labelnum
     f.close()
 
+#返回每种类型图片的个数
 def count_img_type_num(imgrootpath):
     merget_count = []
     for fn in os.listdir(imgrootpath):  # fn 表示的是文件名
@@ -58,14 +81,13 @@ def get_imgpath_and_label( txtpath, txtname):
     return data_dict
 
 if(__name__ == '__main__'):
-        #newpath ='D:\\BigTruck\\new7\\'
-        #path = 'E://data//images//2'
-        #newpath = 'E://data//train//2'
+        newpath = 'E://data//train//2'
         #newpath = 'E://data'
         #images_copy_and_rename(path, newpath, '.jpg',12279)
         #gen_img_label_text( path, newpath ,'11112.txt', ' 1 ')
         #direct_gen_text( '.jpg', [ 12278,24174], newpath, 'train.txt' )
-        direct_gen_text('E://data//images', 'E://data//' )
+        #direct_gen_text('E://data//images', 'E://data//' )
         #data_dict = get_imgpath_and_label(newpath, '9222.txt')
         #print(data_dict)
         #print(count_img_type_num('E://data//images'))
+        #dataproces('E://备份//images_original_2017')
